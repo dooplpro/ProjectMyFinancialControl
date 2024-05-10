ec53f161ddd471c8210cdc457a1124b7615303fb
Diego Ribeiro Martins <diegoribeiromartins@gmail.com>
2024-05-10 13:37:13 -0300

    fix: correct vite version

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import * as reactPlugin from 'vite-plugin-react'
-import type { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-const config: UserConfig = {
-  jsx: 'react',
-  plugins: [reactPlugin]
-}
-
-export default config
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src')
+    }
+  }
+})
