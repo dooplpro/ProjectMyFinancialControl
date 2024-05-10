b6ffaafe9c97c0c1e44f7e877b3005402a9fcb70
Diego Ribeiro Martins <diegoribeiromartins@gmail.com>
2024-05-10 16:50:25 -0300

    chore: prettier eslint and trivago prettier plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,39 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "eslint-config-prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  ignorePatterns: [
+    "dist",
+    ".eslintrc.cjs",
+    "node_modules",
+    "dist",
+    ".prettierrc.cjs",
+    "env.d.ts",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "@trivago/prettier-plugin-sort-imports"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 90,
+  bracketSpacing: true,
+  importOrder: ["^[react]", "^[@]", "^~/(.*)$", "^[./]"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true
+};
