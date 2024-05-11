08dd583d59a3ca236ab5016170e5950b56b90711
Diego Ribeiro Martins <diegoribeiromartins@gmail.com>
2024-05-10 21:47:54 -0300

    chore: firestore

--- src/services/FirebaseService.ts
+++ src/services/FirebaseService.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -13,5 +14,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
+const db = getFirestore(app)
 
-export default { app, auth }
+export default { app, auth, db }
